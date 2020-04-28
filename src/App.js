21734b1746f01f3cb4311afad27104c61525b55b
liyaSileshi <liyasileshi@gmail.com>
2020-04-27 19:25:31 -0700

    fetch works

--- src/App.js
+++ src/App.js
@@ -2,9 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+async function getLyricData(url) {
+  try{
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    const res = await fetch(proxyurl + url)
+    console.log(res)
+    const json = await res.json() 
+    console.log(json)
+    // this.setState({ weatherData: json , isLoading: false})
+    console.log(JSON.parse(json))
+
+  } catch(err) {
+     // If there is no data 
+    //  this.setState({ weatherData: null, errorMessage: err.message }) // Clear the weather data we don't have any to display
+     // Print an error to the console. 
+     console.log('-- Error fetching --')
+     console.log(err.message)
+     // You may want to display an error to the screen here. 
+   }
+}
+
 function App() {
   const apikey = process.env.REACT_APP_LYRICS_API_KEY
-  console.log(apikey)
+  const url = `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=json&callback=callback&q_track=Stay&q_artist=Alessia%20&apikey=${apikey}`
+  getLyricData(url)
+  console.log(url)
   return (
     <div className="App">
       <header className="App-header">

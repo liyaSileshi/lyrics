1d5ed402d9a2a5cf9107d92d57b9c4058cc92620
liyaSileshi <liyasileshi@gmail.com>
2020-05-12 22:01:53 -0700

    Updates

---
+++ precache-manifest.b31bd6e3cf649578a20d8a14e70bcfaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a71334d6612675451424a6fa3ce1dcb0",
+    "url": "/lyrics/index.html"
+  },
+  {
+    "revision": "8b92a0430424c996ad34",
+    "url": "/lyrics/static/css/main.9c73d098.chunk.css"
+  },
+  {
+    "revision": "8cca0c3c2cd1bac0f774",
+    "url": "/lyrics/static/js/2.c9f087ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lyrics/static/js/2.c9f087ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b92a0430424c996ad34",
+    "url": "/lyrics/static/js/main.7d5852dc.chunk.js"
+  },
+  {
+    "revision": "61f16644c01eebcc0e3d",
+    "url": "/lyrics/static/js/runtime-main.55d7df4c.js"
+  }
+]);
